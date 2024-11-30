5c894db300b640768ad658df6dd28ad731fd20de
JumpMaster <kevin@cooper.uk>
2024-11-30 10:04:08 +0000

    StandardFeatures V2 - Stay classy!

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,47 @@
-#define DIAGNOSTIC_PIXEL_PIN   10
-#define DIAGNOSTIC_LED_PIN     11
+#define DIAGNOSTIC_PIXEL
+//#define DIAGNOSTIC_LED_PIN
 
 #include "StandardFeatures.h"
+#include "secrets.h"
+
+StandardFeatures standardFeatures;
+
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+    char data[length + 1];
+    memcpy(data, payload, length);
+    data[length] = '\0';
+
+    Log.printf("mqttCallback - %s : %s\n", topic, data);
+}
+
+void onMQTTConnect() // Called when MQTT is connected or reconnected
+{
+    Log.println("MQTT Connect Callback!");
+
+    standardFeatures.mqttSubscribe("testtopic/#");
+}
+
 
 void setup()
 {
-    StandardSetup();
+    standardFeatures.enableLogging(deviceName, syslogServer, syslogPort);
+    standardFeatures.enableDiagnosticPixel(39);
+    //standardFeatures.enableDiagnosticLed(13); // No LED on Qt Py ESP32-S3
+    standardFeatures.enableWiFi(wifiSSID, wifiPassword, deviceName);
+    standardFeatures.enableOTA(deviceName, otaPassword);
+    standardFeatures.enableSafeMode(appVersion);
+    standardFeatures.enableMQTT(mqttServer, mqttUsername, mqttPassword, deviceName);
+
+    standardFeatures.setMqttOnConnectCallback(onMQTTConnect);
+
+    standardFeatures.setMqttCallback(mqttCallback);
 }
 
 void loop()
 {
-    StandardLoop();
+    standardFeatures.loop();
+
+    if (standardFeatures.isOTARunning())
+        return;
 }
--- src/secrets.h
+++ src/secrets.h
@@ -1,12 +1,15 @@
 #ifndef __SECRETS_H_
 #define __SECRETS_H_
 
+const char* syslogServer = "my.syslog.server";
+const uint16_t syslogPort = 514;
+
 const char* wifiSSID = "WiFi SSID";
 const char* wifiPassword = "MySecureWiFiPassword";
 
-unsigned char mqtt_server[] = {192, 168, 1, 100}; // MQTT Server IP Address
-const char* mqtt_username = "mqtt-username";
-const char* mqtt_password = "mqtt-password";
+unsigned char mqttServer[] = {192, 168, 1, 100}; // MQTT Server IP Address
+const char* mqttUsername = "mqtt-username";
+const char* mqttPassword = "mqtt-password";
 
 const char* otaPassword = "ota-password";
 
